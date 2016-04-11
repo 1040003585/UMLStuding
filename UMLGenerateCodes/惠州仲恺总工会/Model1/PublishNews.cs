68003ddd584842a5b05774dcd560598b0c4ea4da
1040003585 <1040003585@qq.com>
2016-04-11 18:48:04 +0800

    222222222222

---
+++ UMLGenerateCodes/惠州仲恺总工会/Model1/PublishNews.cs
@@ -0,0 +1,48 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**
+ * 
+ */
+public class PublishNews {
+
+    /**
+     * 
+     */
+    public PublishNews() {
+    }
+
+    /**
+     * 
+     */
+    private string newsID;
+
+    /**
+     * 
+     */
+    private string newsTitle;
+
+    /**
+     * 
+     */
+    private string newsContent;
+
+    /**
+     * 
+     */
+    private string pubPeople;
+
+    /**
+     * 
+     */
+    private Date pubTime;
+
+    /**
+     * 
+     */
+    private int flowCompleted;
+
+}
---
+++ UMLGenerateCodes/惠州仲恺总工会/Model1/PublishWorkguide.cs
@@ -0,0 +1,48 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/**
+ * 
+ */
+public class PublishWorkguide {
+
+    /**
+     * 
+     */
+    public PublishWorkguide() {
+    }
+
+    /**
+     * 
+     */
+    private string guideID;
+
+    /**
+     * 
+     */
+    private string guideTitle;
+
+    /**
+     * 
+     */
+    private string guideContent;
+
+    /**
+     * 
+     */
+    private string pubPeople;
+
+    /**
+     * 
+     */
+    private Date pubTime;
+
+    /**
+     * 
+     */
+    private int flowCompleted;
+
+}
